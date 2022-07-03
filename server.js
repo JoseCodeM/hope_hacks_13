72902ba7975e9fbe5470e0a07c7d84bf25461ec3
Jose Eduardo Martinez <joseeduardomartinez@MacBook-Pro-3.local>
2022-07-03 14:57:08 -0400

    update

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser= require('body-parser')
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+const port = 5000;
 const path = require('path');
-// // Static Files
-// app.use(express.static('public'));
-// app.use('/css', express.static(__dirname + 'public/css'))
-// app.use('/js', express.static(__dirname + 'public/js'))
+const req = require('express/lib/request');
+const uri = process.env.MONGODB_CONNECTION_STRING;
+
+
+//Connecting MongoDB to server.
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection
+connection.once("open", () => {
+    console.log("MongoDB connection established")
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Static Files
 app.use(express.static('public'));
@@ -32,12 +48,24 @@ app.get('/contact', (req, res) => {
     res.render('contact',)
 })
 
+app.get('/getResults', (req, res) => {
+    res.render(req.body)
+})
+
 app.get('/quiz', (req, res) => {
     res.render('quiz')
 })
 
+//getting information from the front end
+app.get('/quiz', (req, res) => {
+    req.render('quiz')
+})
+app.post('/getResults', (req, res) => {
+    let post = req.body;
+    const hopehacks13 = Number(post.question1) + Number(post.question2) + Number(post.question3) + Number(post.question4);
 
-
+    console.log(hopehacks13);
+})
 
 
 
