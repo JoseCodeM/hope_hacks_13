499144928da74fdc9b5e0b53312640115ae8ea4f
josh <102491975+joshingcode@users.noreply.github.com>
2022-07-05 15:41:09 -0400

    revised contact

--- public/js/contact.js
+++ public/js/contact.js
@@ -10,3 +10,14 @@ const NavbarDisplayClass = Navbar.classList.contains('display-none');
 NavbarHamburgerToggle.addEventListener('click', () => {
     Navbar.classList.toggle('nav-display-none')
 });
+
+
+// pop box
+
+function openForm() {
+    document.querySelector('#myForm').style.display = "block";
+  }
+  
+  function closeForm() {
+    document.querySelector('#myForm').style.display = "none";
+  } 
